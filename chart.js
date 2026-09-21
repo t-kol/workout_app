7360ba0a7904fef73ed980e9c3eac40d12aa5a25
t-kol <timothy.kolster@gmail.com>
2026-09-20 21:52:37 -0500

    new files

---
+++ chart.js
@@ -0,0 +1,25 @@
+let chartInitialized = false;
+
+document.getElementById('tab-statistics').addEventListener('click', () => {
+  if (!chartInitialized) {
+    const ctx = document.getElementById('myChart');
+    new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+          label: '# of Votes',
+          data: [12, 19, 3, 5, 2, 3],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        responsive: true,
+        scales: {
+          y: { beginAtZero: true }
+        }
+      }
+    });
+    chartInitialized = true;
+  }
+});
